1a4dfd4c98183a33e9bf486319176ab607404236
PsychoPeep <dioogoocardoosoo@gmail.com>
2024-05-31 10:30:56 +0100

    your property page connected to backend

--- src/views/PropertiesView.vue
+++ src/views/PropertiesView.vue
@@ -1,65 +1,60 @@
 <template>
   <main class="py-8 px-4">
-    <h1 class="inter-medium font-color-green font-size-24 page-title">Your properties</h1>
-    <nav id="properties" class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
-      <div v-for="property in properties" :key="property.name">
-        <PropertyContainer :image='property.image' :name='property.name' :location='property.location'
-          :price='property.price' />
+    <h1 class="inter-medium font-color-green font-size-24 page-title">
+      Your properties
+    </h1>
+    <nav
+      id="properties"
+      class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4"
+    >
+      <div v-for="property in properties" :key="property.ID">
+        <PropertyContainer
+          :id="property.ID"
+          :image="getPropertyImage(property)"
+          :name="property.title"
+          :location="property.location"
+          :price="property.daily_price"
+        />
       </div>
     </nav>
   </main>
 </template>
 
 <script>
-import PropertyContainer from '../components/PropertyContainer.vue';
+import { usePropertiesStore } from "@/stores/properties";
+import PropertyContainer from "../components/PropertyContainer.vue";
 export default {
   data() {
     return {
-      properties: [
-        {
-          image: "https://via.placeholder.com/200x200",
-          name: "Beautiful pent-house",
-          location: "Porto, PT",
-          price: 25
-        },
-        {
-          image: "https://via.placeholder.com/200x200",
-          name: "Beautiful pent-house",
-          location: "Lisbon, PT",
-          price: 45
-        },
-        {
-          image: "https://via.placeholder.com/200x200",
-          name: "Beautiful pent-house",
-          location: "Porto, PT",
-          price: 25
-        },
-        {
-          image: "https://via.placeholder.com/200x200",
-          name: "Beautiful pent-house",
-          location: "Lisbon, PT",
-          price: 45
-        },
-        {
-          image: "https://via.placeholder.com/200x200",
-          name: "Beautiful pent-house",
-          location: "Porto, PT",
-          price: 25
-        },
-        {
-          image: "https://via.placeholder.com/200x200",
-          name: "Beautiful pent-house",
-          location: "Lisbon, PT",
-          price: 45
-        },
-      ]
-    }
+      propertiesStore: usePropertiesStore(),
+    };
+  },
+
+  created() {
+    this.propertiesStore.fetchProperties().then(() => {
+      console.log(this.propertiesStore.getProperties);
+    });
+  },
+
+  methods: {
+    getPropertyImage(property) {
+      if (property?.photos?.photo && property.photos.photo.length > 0) {
+        return property.photos.photo[0];
+      }
+      return `https://placehold.co/200x200?text=No%20Image%20for%0A${property.title}`;
+    },
+  },
+
+  computed: {
+    properties() {
+      return this.propertiesStore.getProperties;
+    },
   },
 
   components: {
     PropertyContainer,
-  }
-}
+  },
+};
 </script>
 
 <style scoped>
