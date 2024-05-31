743e5211f4d1f49d18dd629283eebbff383e96d4
Carolina Costa <carolinamccosta@gmail.com>
2024-05-31 11:57:49 +0100

    adding more functions to api file

--- src/api/api.js
+++ src/api/api.js
@@ -47,6 +47,50 @@ export async function post(apiBaseUrl, endpoint, data){
 }
 
 /**
+ * PATCH utility function
+ * @param {string} apiBaseUrl - the base URL from the API
+ * @param {string} endpoint - the endpoint of the API
+ * @param {object} data - the data that will be sent to the server
+ * @returns a promise object with the response
+ */
+export async function patch(apiBaseUrl, endpoint, data) {
+    try {
+        const response = await fetch(`${apiBaseUrl}/${endpoint}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        });
+        return handleResponse(response);
+    } catch (error) {
+        throw error;
+    }
+}
+
+
+/**
+ * DELETE utility function
+ * @param {string} apiBaseUrl - this is the base URL from the API
+ * @param {string} endpoint - this is the endpoint of the API
+ * @returns a promise object with the response
+ */
+export async function remove(apiBaseUrl, endpoint){
+    try {
+        const response = await fetch(`${apiBaseUrl}/${endpoint}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+        return handleResponse(response);
+    } catch (error) {
+        throw error;
+    }
+}
+
+
+/**
  * Response handling 
  * @param {object} response a promise object with the response of the server
  * @returns the response from the server 
