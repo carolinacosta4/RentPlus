31a743b3ed8b50f5b6a5533b5af4b70b5bc365ff
Carolina Costa <carolinamccosta@gmail.com>
2024-06-06 15:20:54 +0100

    property page DONE

--- src/views/PropertyView.vue
+++ src/views/PropertyView.vue
@@ -35,10 +35,22 @@
             property.bathrooms }}
             bedrooms | {{ property.beds }} beds | {{ property.guest_number }} guests</p>
           <div id="book">
-            <input class="w-8/12 border-x px-4 inter-light font-color-green" type="date" v-model="dateIn" :min="minDate"
-              id="dateIn" />
-            <input class="w-8/12 border-x px-4 inter-light font-color-green" type="date" :min="minDate"
-              v-model="dateOut" />
+            <div class="calendar">
+              <input type="text" class="w-1/2 border-x px-4 inter-light font-color-green" v-model="formattedDateIn"
+                disabled>
+              <Calendar @click="openCalendarIn" />
+              <div id="datepickIn" v-if="isCalendarIn">
+                <VDatePicker :attributes="attributes" id="calendar" @mouseleave="openCalendarIn" v-model="dateIn" />
+              </div>
+            </div>
+            <div class="calendar">
+              <input class="w-1/2 border-x px-4 inter-light font-color-green" type="text" v-model="formattedDateOut"
+                disabled>
+              <Calendar @click="openCalendarOut" />
+              <div id="datepickOut" v-if="isCalendarOut">
+                <VDatePicker :attributes="attributes" id="calendar" @mouseleave="openCalendarOut" v-model="dateOut" />
+              </div>
+            </div>
             <div id="guests">
               <input class="w-5/12 border-x px-4 inter-light font-color-green" type="number"
                 :max="this.property.guest_number" min="1" placeholder="Guests" v-model="nrGuests" required />
@@ -50,7 +62,28 @@
           <p id="total" class="font-size-18 inter-light font-color-black">
             <span class="inter-medium">Total</span> {{ total }}€
           </p>
-          <button class="button-green" id="rentBtn" @click="rentProperty">Rent</button>
+          <v-dialog max-width="500" v-model="isActiveDialog">
+            <template v-slot:activator="{ props: activatorProps }">
+              <button v-bind="activatorProps" class="button-green" id="rentBtn">Rent</button>
+            </template>
+            <template v-slot:default="{ isActive }">
+              <v-card>
+                <v-card-text>
+                  <h1 class="page-title font-size-14 modalTitle inter-light">Choose the payment method</h1>
+                  <select id="paymentTypes" v-model="selectedMethod">
+                    <option value="0">Select a method</option>
+                    <option v-for="paymentType in paymentTypes" :value="paymentType.ID">{{ paymentType.type }}</option>
+                  </select>
+                </v-card-text>
+                <v-card-actions>
+                  <div class="btnsModal">
+                    <button class="inter-medium button-green" @click="rentProperty">Rent!</button>
+                    <button class="inter-medium button-border-green" @click="isActive.value = false">Cancel</button>
+                  </div>
+                </v-card-actions>
+              </v-card>
+            </template>
+          </v-dialog>
         </div>
       </div>
     </div>
@@ -100,12 +133,8 @@
       </div>
     </div>
     <h3 class="font-size-20 inter-medium font-color-green page-title">Where you will be</h3>
-    <!-- <iframe :src="property.map_url" height="400" style="border:0;" allowfullscreen="" loading="lazy"
-            referrerpolicy="no-referrer-when-downgrade"></iframe> -->
-    <iframe
-      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d49666.92346360088!2d-104.93549284333204!3d38.919801040086455!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8713517510b3e71d%3A0x129d2eeec706b3b0!2sNorthwest%20Colorado%20Springs%2C%20Colorado%20Springs%2C%20CO%2C%20EUA!5e0!3m2!1spt-PT!2spt!4v1716240879380!5m2!1spt-PT!2spt"
-      width="600" height="450" style="border: 0" allowfullscreen="" loading="lazy"
-      referrerpolicy="no-referrer-when-downgrade"></iframe>
+    <iframe :src="property.map_url" height="400" style="border:0;" allowfullscreen="" loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"></iframe>
     <h3 class="font-size-20 inter-medium font-color-green page-title">What the guests say</h3>
     <div id="reviews">
       <div id="allReviews" v-if="reviews.length > 0">
@@ -161,11 +190,11 @@
             less -</p>
         </div>
         <div v-else style="margin-top: 2em"></div>
-        <router-link :to="{ name: 'messages' }"><button class="button-green" id="msgBtn">Message {{ owner.username
+        <router-link v-if="owner.username != loggedUser" :to="{ name: 'messages', params: { id: owner.username } }"><button class="button-green"
+            id="msgBtn">Message {{ owner.username
             }}</button></router-link>
       </div>
     </div>
-    <VCalendar :initial-page="{ month: new Date().getMonth() + 1, year: 2024 }" :attributes="attributes" />
   </main>
 </template>
 
@@ -175,10 +204,12 @@ import ArrowRight from "vue-material-design-icons/ArrowRight.vue";
 import Guests from "vue-material-design-icons/AccountPlusOutline.vue";
 import Star from "vue-material-design-icons/Star.vue";
 import Television from "vue-material-design-icons/Television.vue";
+import Calendar from "vue-material-design-icons/Calendar.vue";
 import { usePropertiesStore } from "@/stores/properties";
 import { useReviewsStore } from "@/stores/reviews";
 import { useUsersStore } from "@/stores/users";
 import { useReservationsStore } from "@/stores/reservations";
+import { usePaymentTypesStore } from "@/stores/paymentTypes";
 import { ref } from 'vue';
 
 export default {
@@ -191,6 +222,9 @@ export default {
       ],
       dateOut: "",
       dateIn: "",
+      formattedDateOut: "00/00/0000",
+      formattedDateIn: "00/00/0000",
+      selectedMethod: 0,
       nrGuests: 1,
       readMore: false,
       readMoreMeet: false,
@@ -202,12 +236,16 @@ export default {
       reviewsStore: useReviewsStore(),
       usersStore: useUsersStore(),
       reservationsStore: useReservationsStore(),
+      paymentTypesStore: usePaymentTypesStore(),
       ownerFetched: false,
       errorMessage: "",
       sucessfullMessage: "",
       disabledDates: [],
-      minDate: new Date().toISOString().split("T")[0],
-      attributes: null
+      minDate: new Date(),
+      attributes: null,
+      isCalendarIn: false,
+      isCalendarOut: false,
+      isActiveDialog: false
     };
   },
 
@@ -217,6 +255,7 @@ export default {
     Guests,
     Star,
     Television,
+    Calendar
   },
 
   async created() {
@@ -225,7 +264,7 @@ export default {
     await this.usersStore.fetchUser(this.property.owner_username);
     await this.usersStore.fetchUserReviews(this.property.owner_username);
     await this.reservationsStore.fetchReservationsPerProperty(this.$route.params.id);
-    console.log(new Date(2018, 0, 15));
+    await this.paymentTypesStore.fetchPaymentTypes()
     this.computeDisabledDates()
   },
 
@@ -249,8 +288,8 @@ export default {
         return 0;
       }
 
-      const dateIn = new Date(this.dateIn);
-      const dateOut = new Date(this.dateOut);
+      const dateIn = this.dateIn;
+      const dateOut = this.dateOut;
       const timeDiff = dateOut.getTime() - dateIn.getTime();
       const diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
       return diffDays * this.property.daily_price;
@@ -315,6 +354,14 @@ export default {
         return 0;
       }
     },
+
+    paymentTypes() {
+      return this.paymentTypesStore.getTypes;
+    },
+
+    loggedUser(){
+      return this.usersStore.getUserLogged
+    }
   },
 
   methods: {
@@ -333,6 +380,14 @@ export default {
           ])
         }
       });
+    },
+
+    openCalendarIn() {
+      return [this.isCalendarIn = !this.isCalendarIn, this.isCalendarOut = false]
+    },
+
+    openCalendarOut() {
+      return [this.isCalendarOut = !this.isCalendarOut, this.isCalendarIn = false]
     },
 
     nextPage(n) {
@@ -360,19 +415,23 @@ export default {
     },
 
     async rentProperty() {
-      console.log(this.dateIn, this.dateOut, this.total);
-      if (!this.dateIn || !this.dateOut || this.total <= 0) {
+      const dtIn = new Date(this.dateIn)
+      const dtOut = new Date(this.dateOut)
+      if (!this.formattedDateIn || !this.formattedDateOut || this.total <= 0) {
         this.errorMessage = 'Invalid booking details'
-        return
+        this.isActiveDialog = false
+      } else if (this.selectedMethod == 0) {
+        this.errorMessage = 'You need to select a payment method!'
+        this.isActiveDialog = false
       } else {
         try {
           await this.reservationsStore.createReservation({
             "property_ID": this.property.ID,
-            "dateIn": this.dateIn,
-            "dateOut": this.dateOut,
+            "dateIn": `${dtIn.getFullYear()}-${dtIn.getMonth() + 1}-${dtIn.getDate()}`,
+            "dateOut": `${dtOut.getFullYear()}-${dtOut.getMonth() + 1}-${dtOut.getDate() + 1}`,
             "total_price": this.total,
-            "username": "carolina",
-            "payment_type": 1
+            "username": this.loggedUser,
+            "payment_type": this.selectedMethod
           })
 
           this.sucessfullMessage = 'Reservation Successful'
@@ -380,13 +439,33 @@ export default {
         } catch (error) {
           if (error == 'Error: API request failed with status 400: {"success":false,"error":"Invalid date","msg":"You can only make reservations for future days"}') {
             this.errorMessage = ''
+            this.sucessfullMessage = ''
             this.errorMessage = 'You can only make reservations for future days.';
           } else if (error == 'Error: API request failed with status 400: {"success":false,"error":"Property already booked","msg":"You cant proceed with the reservation because there is already a reservation during the chosen dates."}') {
             this.errorMessage = ''
+            this.sucessfullMessage = ''
             this.errorMessage = 'There is already a reservation during the chosen dates.';
           }
         }
+        this.isActiveDialog = false
       }
+    },
+
+    formatDate(date) {
+      const d = new Date(date)
+      const year = d.getFullYear()
+      const month = String(d.getMonth() + 1).padStart(2, '0')
+      const day = String(d.getDate()).padStart(2, '0')
+      return `${day}-${month}-${year}`
+    },
+  },
+
+  watch: {
+    dateIn(newValue) {
+      this.formattedDateIn = this.formatDate(newValue);
+    },
+    dateOut(newValue) {
+      this.formattedDateOut = this.formatDate(newValue);
     }
   },
 };
@@ -403,12 +482,12 @@ export default {
 #book {
   display: flex;
   flex-direction: column;
-  padding: 1nrem;
+  padding: 0 1.5rem;
+  justify-content: center;
   background-color: #f2f2f2;
   border-radius: 11px;
   margin: 2rem 0;
   height: 50%;
-  row-gap: 1rem;
 }
 
 #dateIn {
@@ -419,6 +498,7 @@ export default {
   display: flex;
   flex-direction: row;
   column-gap: 2rem;
+  margin-top: 1em;
 }
 
 #total {
@@ -543,6 +623,7 @@ iframe {
 #hostInfo {
   display: grid;
   grid-template-columns: 1fr 1fr;
+  align-items: center;
 }
 
 #reviewsPhoto {
@@ -619,5 +700,41 @@ iframe {
 
 #sucessfullMessage {
   color: green;
+}
+
+.calendar {
+  display: flex;
+  flex-direction: row;
+  margin-top: 1em;
+}
+
+#datepickIn,
+#datepickOut {
+  position: absolute;
+  z-index: 9999;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+  border-radius: 8px;
+  margin-top: 2rem;
+}
+
+.btnsModal {
+  display: flex;
+  flex-direction: row;
+  padding: 0.5rem 1rem 0.13rem;
+  column-gap: 1rem;
+}
+
+#inputs {
+  display: flex;
+  flex-direction: column;
+}
+
+#paymentTypes {
+  width: 90%;
+  border: #133e1a20 solid 1px;
+  border-radius: 11px;
+  padding: 0.5em;
+  padding-left: 0.5em;
+  border-radius: 5px;
 }
 </style>
